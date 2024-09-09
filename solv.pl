c0874b0e7c1991b93bb088cc241dc1c4c47c901b
mike potanin <mpotanin@gmail.com>
2024-09-09 21:53:28 +0300

    Theories

--- solv.pl
+++ solv.pl
@@ -1,10 +1,10 @@
 
 ap(A -> B, A, B).
 
-axiom(A -> (_ -> A), "A->B->A").
-axiom((A -> (B -> C)) -> ((A -> B) -> (A -> C)), "(A->B->C)->(A->B)->A->C").
-axiom((~(A) -> ~(B)) -> (B -> A), "(~A->B)->(B->A)").
-axiom(A) :- axiom(A, _).
+axiom(hilbert_pos, A -> (_ -> A), "A->B->A").
+axiom(hilbert_pos, (A -> (B -> C)) -> ((A -> B) -> (A -> C)), "(A->B->C)->(A->B)->A->C").
+axiom(hilbert, (~(A) -> ~(B)) -> (B -> A), "(~A->~B)->(B->A)").
+axiom(Theories, A) :- axiom(Theory, A, _), member(Theory, Theories).
 
 nsplit(1, 0, 0).
 nsplit(N, X, Y) :- N > 1,
@@ -15,15 +15,15 @@ nsplit(N, X, Y) :- N > 1,
                      X = XO, Y is YO+1
                    ).
 
-solve(P, C, C, _, A, A) :- member(P, C), !.
-solve(P, C, [P|C], _, A, A) :- axiom(P).
-solve(P, C, T, N, Aps, [X | Apso]) :- N > 0,
+solve(Theories, P, C, [P|C], _, A, A) :- axiom(Theories, P).
+solve(_, P, C, C, _, A, A) :- member(P, C).
+solve(Theories, P, C, T, N, Aps, [X | Apso]) :- N > 0,
                      ap(X, Y, P),
 %                      nsplit(N, A, B),
                      A is N-1, B = A,
-                     solve(X, C, TA, A, [], Aps1),
+                     solve(Theories, X, C, TA, A, [], Aps1),
+                     solve(Theories, Y, TA, TB, B, [], Aps2),
                      acyclic_term(X),
-                     solve(Y, TA, TB, B, [], Aps2),
                      append([Aps2, Aps1, Aps], Apso),
                      T = [P | TB].
 
@@ -33,7 +33,7 @@ marks([V|T], [(N,V)|TM], N) :- N1 is N+1, marks(T, TM, N1).
 marks(I, O) :- marks(I, O, 0).
 
 get(N, F, L) :- member((N, (_ -> F)), L), !.
-get(N, F, _) :- axiom(F, N), !.
+get(N, F, _) :- axiom(_, F, N), !.
 get(F, F, _).
 
 show([], _).
@@ -43,11 +43,13 @@ show([(N, (A -> B)) | T], L) :- get(NA, A, L),
                            show(T).
 show(L) :- show(L, L).
 
-run(P, N, R, A) :- solve(P, [], R, N, [], A), acyclic_term(R).
-run(P, N, L) :- run(P, N, _, A),
+run(Theories, P, N, R, A) :- solve(Theories, P, [], R, N, [], A)
+                   , acyclic_term(R)
+                   .
+run(Theories, P, N, L) :- run(Theories, P, N, _, A),
              marks(A, AM),
              length(AM, L),
              show(AM).
 
-% run((a -> (b -> c)) -> (b -> (a -> c)), 6, L).
-% run((~(a) -> a) -> a, 6, L).
+% run([hilbert_pos], (a -> (b -> c)) -> (b -> (a -> c)), 6, L).
+% run([hilbert_pos, hilbert], (~(a) -> a) -> a, 5, L).
